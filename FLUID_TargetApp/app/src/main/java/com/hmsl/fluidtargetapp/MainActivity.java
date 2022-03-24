00e89e2705bf9c43c8ee16717e99d179ccd87302
sangjin-hash <lsj9873@ajou.ac.kr>
2022-03-24 20:09:23 +0900

    Input Apk

--- FLUID_TargetApp/app/src/main/java/com/hmsl/fluidtargetapp/MainActivity.java
+++ FLUID_TargetApp/app/src/main/java/com/hmsl/fluidtargetapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.hmsl.fluidtargetapp;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,7 +10,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -23,21 +21,21 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "TEST";
 
-    private static Bitmap[] sampleArr = new Bitmap[10];
-    private static int index = 0;
+    static Bitmap[] sampleArr = new Bitmap[10];
+    static int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sample = (ImageView)findViewById(R.id.sample);
-        btn_prev = (Button)findViewById(R.id.btn_prev);
-        btn_next = (Button)findViewById(R.id.btn_next);
+        sample = (ImageView) findViewById(R.id.sample);
+        btn_prev = (Button) findViewById(R.id.btn_prev);
+        btn_next = (Button) findViewById(R.id.btn_next);
 
         AssetManager am = getResources().getAssets();
         InputStream is = null;
-        try{
+        try {
             is = am.open("0.jpg");
             Bitmap bm = BitmapFactory.decodeStream(is);
             init_bitmap();
@@ -59,26 +57,18 @@ public class MainActivity extends AppCompatActivity {
         btn_prev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(index != 0){
-                    index--;
-                    sample.setImageBitmap(sampleArr[index]);
-                }else{
-                    index = 9;
-                    sample.setImageBitmap(sampleArr[index]);
-                }
+                index_dec();
+                sample.setImageBitmap(sampleArr[index]);
+                return;
             }
         });
 
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(index != 9){
-                    index++;
-                    sample.setImageBitmap(sampleArr[index]);
-                }else{
-                    index = 0;
-                    sample.setImageBitmap(sampleArr[index]);
-                }
+                index_inc();
+                sample.setImageBitmap(sampleArr[index]);
+                return;
             }
         });
     }
@@ -86,9 +76,9 @@ public class MainActivity extends AppCompatActivity {
     private void init_bitmap() {
         AssetManager am = getResources().getAssets();
         InputStream is = null;
-        try{
-            for(int i = 0; i < 10; i++){
-                is = am.open(""+i+".jpg");
+        try {
+            for (int i = 0; i < 10; i++) {
+                is = am.open("" + i + ".jpg");
                 Bitmap bm = BitmapFactory.decodeStream(is);
                 sampleArr[i] = bm;
             }
@@ -96,5 +86,19 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    static void index_inc() {
+        if (index != 9)
+            index++;
+        else
+            index = 0;
+    }
+
+    static void index_dec() {
+        if (index != 0)
+            index--;
+        else
+            index = 9;
     }
 }
